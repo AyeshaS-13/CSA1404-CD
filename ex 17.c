8586d55c93f5cbf7580acaba6356f02800aaa7f3
AyeshaS-13 <ayeshasiddiqas4305.sse@saveetha.com>
2025-11-18 14:03:00 +0530

    Create ex 17.c

---
+++ ex 17.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int words = 0, lines = 1, characters = 0;
+
+    printf("Enter text (end with ~):\n");
+
+    scanf("%[^~]", str);   // Read until ~
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (str[i] == ' ') {
+            words++;
+        }
+        else if (str[i] == '\n') {
+            lines++;
+            words++;   // New line means new word starts
+        }
+        else {
+            characters++;
+        }
+    }
+
+    if (characters > 0)
+        words++;
+
+    printf("Total number of words : %d\n", words);
+    printf("Total number of lines : %d\n", lines);
+    printf("Total number of characters : %d\n", characters);
+
+    return 0;
+}
